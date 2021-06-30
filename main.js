c038a62e630d10dd1de8a51d84bb3dbf4050d81a
JoeyTheHarryPotter <74834315+JoeyTheHarryPotter@users.noreply.github.com>
2021-06-30 14:29:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+song = "";
+
+function preload(){
+    song = loadSound("Clubstep Full Version.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(500, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+
+    poseNet.on('pose', gotPoses);
+
+    song.play();
+
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function draw(){
+    image(video, 0, 0, 500, 500);
+}
+
+function modelLoaded(){
+    console.log("PoseNet Initialized!");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+
+        console.log("Left Wrist X Position: " + leftWristX + " Left Wrist Y Position: " + leftWristY);
+        console.log("Right Wrist X Position: " + rightWristX + " Right Wrist Y Position: " + rightWristY);
+    }
+}
